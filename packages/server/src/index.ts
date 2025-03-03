a7ac96bcc750195bd2dcefdd5c0f685bb5b2ed71
iris <iris@heyiris.dev>
2025-03-03 00:03:46 +0000

    feat(server): don't show error to user

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -14,11 +14,22 @@ const appLogger = new Logger("app");
 async function startServer() {
   const app = new Elysia()
     .use(swagger())
+    .onError(({ code, error, set }) => {
+      set.status = 500;
+      appLogger.error("error:", error);
+      return {
+        status: "error",
+        code: 500,
+      };
+    })
     .post(
       "/users",
       async ({ body }) => {
         const user = await db.insert(schema.users).values(body).returning();
-        return user[0];
+        return {
+          status: "success",
+          data: user[0],
+        };
       },
       {
         body: models.user.create,

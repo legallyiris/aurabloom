b4c3e5ca28a7dd5487fd39cd49fde01de77c3c1f
iris <iris@heyiris.dev>
2025-03-06 22:41:36 +0000

    feat(client): initialise vue project

---
+++ packages/client/README.md
@@ -0,0 +1,7 @@
+# @aurabloom/client
+
+this is the web client for aurabloom, it is built with vue.js and typescript.
+we use elysia's [eden treaty] for interaction with the backend.
+
+
+[eden treaty]: https://elysiajs.com/eden/treaty/overview.html
---
+++ packages/client/src/main.ts
@@ -0,0 +1,20 @@
+import "./assets/main.css";
+
+import type { App as AurabloomApp } from "@aurabloom/server";
+import { treaty } from "@elysiajs/eden";
+
+import { createPinia } from "pinia";
+import { createApp } from "vue";
+
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+app.config.globalProperties.$api = treaty<AurabloomApp>(
+  "http://localhost:3000",
+);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");

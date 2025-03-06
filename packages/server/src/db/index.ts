a699053b0d26816f74340a0f523dda39edd90e81
iris <iris@heyiris.dev>
2025-03-06 17:43:48 +0000

    feat(server): warn if the db is empty

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -1,15 +1,29 @@
 import { Database } from "bun:sqlite";
-import { Logger } from "common";
+import fs from "node:fs";
+import { Logger } from "@aurabloom/common";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 
 import config from "../config";
 import * as schema from "./schema";
 
+function helper(message: string) {
+  logger.error(message);
+  logger.error("please run the following commands:");
+  logger.error("\tbunx drizzle-kit up");
+  logger.error("\tbunx drizzle-kit migrate");
+  console.log();
+  process.exit(1);
+}
+
 const logger = new Logger("db");
 
-const sqlite = new Database(config.db.path, { create: true });
+if (!fs.existsSync(config.db.path)) helper("database file not found");
+
+const sqlite = new Database(config.db.path);
 const db = drizzle(sqlite, { schema });
-logger.info(`Database file: ${sqlite.filename}`);
+
+const tables = db.all("SELECT name FROM sqlite_master WHERE type='table'");
+if (tables.length <= 1) helper("database is empty");
 
 export { schema };
 export default db;
